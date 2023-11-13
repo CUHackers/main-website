c4dd3bcb75c6f95976da327064ef95b519a9b24a
Nathan Brown <43689712+OxxoCodes@users.noreply.github.com>
2023-11-12 20:26:39 -0500

    Added partnership information to Partners page. Re-added Partners link to Navbar.

--- src/Components/Partners/Partners.js
+++ src/Components/Partners/Partners.js
@@ -1,24 +1,36 @@
 import './Partners.scss';
-import partnersContent from './partners.json';
-//import faqbg from './faqbg2.png';
 
 function Partners() {
-
-    const partnersArray = Array.from(partnersContent);
-  
-    const partnersMap = partnersArray.map((question, index) =>
-      <div className="question">
-        <div className="questionq">{question.question}</div>
-        <div className="questiona">{question.answer}</div>
-      </div>
-    );
-  
     return(
       <div id="faq" >
         <div className="partnersBody">
-          <h2 className="partnersHead">Interested in partnering with CUHackit?</h2>
+          <h2 className="partnersHead">Interested in partnering with CUhackit?</h2>
+          <h3 className="partnersHeadPartnershipPacket">Check out our partnership prospectus packet <a target="_blank" href="https://drive.google.com/file/d/1SLkB8gVhy-ERrtgkAk3cu_hkraYbHqNY/view?usp=sharing">here!</a></h3>
           <div id="partnersBlock">
-            {partnersMap}
+            <div className="question">
+              <div className="questionq">What is CUhackit?</div>
+              <div className="questiona">CUhackit is Clemson University's official hackathon organization! We are entirely student-run, and we host two hackathons each year: CUhackit, our flagship hackathon, and HelloWorld, our freshmen-only hackathon. Our CUhackit event is the largest hackathon in all of South Carolina! In addition, we are working to bring back BothoHacks, our hackathon in Botswana, in partnership with Botho University!</div>
+            </div>
+            <div className="question">
+              <div className="questionq">Why should our organization partner with CUhackit?</div>
+              <div className="questiona">Partnering with CUhackit gives you and your organization the opportunity to connect with some of Clemson's most talented and driven students, increase your brand visibility, recruit future talent, and help contribute to the cultivation of future engineers and leaders. It's also a chance to support innovation and technological development in the academic community.</div>
+            </div>
+            <div className="question">
+              <div className="questionq">What partnership opportunities are available?</div>
+              <div className="questiona">We offer various partnership opportunities, including event sponsorship, workshop presentations, judging, mentorship, and more! For more information, check out our partnership prospectus packet.</div>
+            </div>
+            <div className="question">
+              <div className="questionq">How can our organization contribute to CUhackit?</div>
+              <div className="questiona">Organizations can contribute through financial sponsorship, providing meals or hardware to hackers, donating prizes, and more! Each form of contribution plays a vital role in the success of CUhackit.</div>
+            </div>
+            <div className="question">
+              <div className="questionq">What benefits do partners receive?</div>
+              <div className="questiona">Partners receive a range of benefits such as brand exposure to a diverse group of students, networking opportunities with potential recruits, incorporating your organization's products and services into hackathon \"Challenges,\" showcasing your organization's commitment to education and innovation, and firsthand access to hacker resumes, GitHub and LinkedIn profiles, and projects/hacks. For more information on the different benefits offered, please reference our partnership prospectus packet.</div>
+            </div>
+            <div className="question">
+              <div className="questionq">How do we become a partner?</div>
+              <div className="questiona">To become a partner, please get in touch with us at cuhackers@clemson.edu. We will discuss the partnership levels, benefits, and how we can tailor our partnership to meet your organization's goals. Thank you for helping make CUhackit a reality and for helping foster student innovation!</div>
+            </div>
           </div>
         </div>
       </div>
