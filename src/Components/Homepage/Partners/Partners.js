b289e19d2401ba908ef96de387c73e3967130730
Nathan Brown <43689712+OxxoCodes@users.noreply.github.com>
2024-02-28 01:13:42 -0500

    Update partner logos

--- src/Components/Homepage/Partners/Partners.js
+++ src/Components/Homepage/Partners/Partners.js
@@ -1,12 +1,13 @@
 import './Partners.scss';
-import logo from './SoCLogo.png';
 import aws_logo from './new/AWS_Logo.png';
 import ge from './new/ge_monogram_dark gray_432.png';
 import partnerdevice from './new/cactus.png';
 import GiantOak from "./new/Giant Oak.png"
 import Postman from "./new/postman.svg";
-import SoC from "./new/Clemson School of Computing.png"
-import logo_watt from './logo_watt.png';
+import SoC from "./new/SoC-Orange-and-Purple.png"
+import logo_watt from './new/Watt.png';
+import rtx from "./new/rtx_logo.png";
+import deloitte from "./new/deloitte.png";
 
 ge ? console.log(ge): console.log("no dice");
 
@@ -18,10 +19,12 @@ function Partners() {
         <div className="partnertextbody">
           <h2 className="partnerhead">in partnership with</h2>
           <div className="thepartner">
-            <a href="https://www.clemson.edu/cecas/departments/computing/" target="_blank"><div><img src={logo} alt="Clemson University School of Computing Logo"/></div></a>
-            <a href="https://www.clemson.edu/centers-institutes/watt/" target="_blank"><div><img src={logo_watt} alt="Watt Family Innovation Center Logo"/></div></a>
             <a href="https://aws.amazon.com/" target="_blank"><div><img src={aws_logo} alt="AWS Logo"/></div></a>
+            <a href="https://www.clemson.edu/centers-institutes/watt/" target="_blank"><div><img src={logo_watt} alt="Watt Family Innovation Center Logo"/></div></a>
+            <a href="https://www.clemson.edu/cecas/departments/computing/" target="_blank"><div><img src={SoC} alt="Clemson University School of Computing Logo"/></div></a>
             <a href="https://www.giantoak.com" target="_blank"><div><img src={GiantOak} alt="Giant Oak Logo"/></div></a>
+            <a href="https://www.rtx.com/" target="_blank"><div><img src={rtx} alt="Raytheon Logo"/></div></a>
+            <a href="https://www2.deloitte.com/us/en.html" target="_blank"><div><img src={deloitte} alt="Deloitte Logo"/></div></a>
             {/* <a href="https://www.clemson.edu/cecas/departments/computing/index.html" target="_blank"><div><img src={SoC} alt="Clemson School of Computing Logo"/></div></a> */}
           </div>
         </div>
